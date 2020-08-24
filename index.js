b6d27cd46b27266ca27cf9e8e84e7953d915df51
윤희제 <n4500616@gmail.com>
2020-08-24 14:58:41 +0900

    bot upload

--- index.js
+++ index.js
@@ -10,16 +10,6 @@ client.on('ready', () => {
   console.log('켰다.');
 });
 
-client.on("guildMemberAdd", (member) => {
-  const guild = member.guild;
-  const newUser = member.user;
-  const welcomeChannel = guild.channels.find(channel => channel.name == welcomeChannelName);
-
-  welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
-
-  member.addRole(guild.roles.find(role => role.name == "게스트"));
-});
-
 client.on("guildMemberRemove", (member) => {
   const guild = member.guild;
   const deleteUser = member.user;
