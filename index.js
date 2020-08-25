d8a4c924f14909bbf87458733eda164f7d8983b4
윤희제 <n4500616@gmail.com>
2020-08-25 13:58:21 +0900

    add userinfo

--- index.js
+++ index.js
@@ -41,6 +41,19 @@ client.on('message', (message) => {
     message.reply('http://promise.devteam.r-e.kr/')
   }
 
+  if(message.content == '!userinfo') {
+    let embed = new Discord.RichEmbed()
+    let img = 'https://media.discordapp.net/attachments/746880838355910786/746921925577670686/d40f2b05690f8590.png?width=424&height=424';
+    embed.setColor('9d1be8')
+    embed.setFooter(`Promise Dev Team`)
+    embed.addBlankField()
+    embed.addField('이름:', '${mentionedUser.username}')
+    embed.addField('ID:', '${mentionedUser.id}')
+    embed.addField('계정생성일:', '${mentionedUser.createdAt}')
+    embed.addField('서버 접속일:', '${mentionedMember.joinedAt}')
+    embed.addField('프로필:', '${mentionedUser.profile}')
+  }
+
   if(message.content == '!si') {
     let embed = new Discord.RichEmbed()
     let img = 'https://media.discordapp.net/attachments/746880838355910786/746921925577670686/d40f2b05690f8590.png?width=424&height=424';
